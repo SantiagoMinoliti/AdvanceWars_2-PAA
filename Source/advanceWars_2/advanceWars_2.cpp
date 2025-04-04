942bb3749d7381361299ce4ba07d676af4a380a3
Santiago-Minoliti <santiagominoliti.work@gmail.com>
2025-04-04 16:06:53 +0200

    initial commit

---
+++ Source/advanceWars_2/advanceWars_2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "advanceWars_2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, advanceWars_2, "advanceWars_2" );
